b384b21dc7661105a08c31aa73054fca09b35576
Daniel Corbe <daniel@corbe.net>
2026-04-13 18:46:48 -0400

    feat: replace shift/tab mod-tap with plain shift on base layer, add explicit tab to nav layer

--- keyboards/zsa/voyager/keymaps/dan/keymap.c
+++ keyboards/zsa/voyager/keymaps/dan/keymap.c
@@ -112,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MO(_NAV),       KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,
         KC_LEFT_GUI,    KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_QUOTE,
         KC_LEFT_CTRL,   KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       TD(TD_LAYER_TOGGLE),
-                                                        MT(MOD_LALT, KC_BSPC), MT(MOD_LSFT, KC_TAB),                                   MT(MOD_RSFT, KC_ENTER), MT(MOD_RALT, KC_SPACE)
+                                                        MT(MOD_LALT, KC_BSPC), KC_LSFT,                                        MT(MOD_RSFT, KC_ENTER), MT(MOD_RALT, KC_SPACE)
     ),
 
     [_NAV] = LAYOUT(
@@ -120,7 +120,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,        _______,        _______,        _______,        _______,        _______,                                        _______,        _______,        _______,        _______,        _______,        _______,
         _______,        _______,        _______,        _______,        _______,        _______,                                        KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       _______,        _______,
         _______,        _______,        _______,        _______,        _______,        _______,                                        _______,        _______,        _______,        _______,        _______,        _______,
-                                                        _______,        _______,                                                        _______,        _______
+                                                        _______,        KC_TAB,                                                         _______,        _______
     ),
 
     // EVE Online module layer
